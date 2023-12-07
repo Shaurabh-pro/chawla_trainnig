908b8e259845c4b93c30a3f42d81ede14b1cb41e
Shaurabh pro <sorabhkumar604@gmail.com>
2023-12-07 17:21:00 +0530

    second commit

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ fetch("https://jsonplaceholder.typicode.com/posts", {
 
 
     // file upload using java script 
-   const inpFile = document.getElementById('inpFile');
+   /*const inpFile = document.getElementById('inpFile');
    inpFile.addEventListener("change", function(){
     console.log(inpFile.files);
     for(const file of inpFile.files){
@@ -29,6 +29,55 @@ fetch("https://jsonplaceholder.typicode.com/posts", {
         }
     }
 
-   })
+   })*/
+
+   //rest operator
+   /*function sum(name, ...args){
+    console.log(arguments);
+    document.write(`hello ${name}`)
+    let sum = 0;
+    for(let i in args)
+    sum += args[i];
+    document.write(sum + " <br>");
+   }
+   sum("sam", 20,40)
+   sum("amit", 20,4,6)*/
+
+   //spread operator
+   /*function sum(name, ...args){
+    console.log(arguments);
+    document.write(`hello ${name}`)
+    let sum = 0;
+    for(let i in args)
+    sum += args[i];
+    document.write(sum + " <br>");
+   }
+   let arr = [20,40];
+   sum("sam", ...arr);*/
+
+   //destructuring array
+   /*let users = ["amit", 22, "haridwar",["male", 25000]];
+   let [name, age, city,[gender, salary]] = users;
+   console.log(name, age, city, gender, salary);*/
+
+   let product =[
+    {
+        name: "sam",
+        price: 6999
+    },
+    {
+        name: "amit",
+        price: 8999
+    },
+    {
+        name: "shivam",
+        price: 7999
+    },
+
+
+   ];
+
+   console.table(product);
+
 
 
